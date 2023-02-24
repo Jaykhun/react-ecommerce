08e83b6dfefdcfd8535a54f53450505b55f3e20b
jaykhun <dosibaevjaykhun@gmail.com>
2023-02-24 19:27:08 +0300

    changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {Provider} from "react-redux";
-import {store} from "./store/store";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from "react-redux"
+import App from './App'
+import { store } from "./store/store"
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
-);
+)
 
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App/>
+            <App />
         </Provider>
     </React.StrictMode>
-);
+)
