392eea21d1316e7f7fafa55d514e7871b7b42daf
jaykhun <dosibaevjaykhun@gmail.com>
2023-02-25 14:42:33 +0300

    added save function in cookie

--- src/components/Popup/SignInPopup/SignInPopup.tsx
+++ src/components/Popup/SignInPopup/SignInPopup.tsx
@@ -1,42 +1,39 @@
-import Popup from "../../UI/Popup/Popup";
-import {useActions} from "../../../hooks/useActions";
-import {useTypedSelector} from "../../../hooks/useTypedSelector";
-import {useLoginUserMutation} from "../../../store/user/userApi";
-import {SubmitHandler, useForm} from "react-hook-form";
-import {LoginType} from "../../../store/user/userTypes";
-import {ErrorMessage} from "@hookform/error-message";
-import {ActionAlert, ActionLoader, InputError} from "../../UI";
-import jwt from "jwt-decode";
-import "./SignInPopup.scss";
-import "../../UI/Popup/Popup.scss";
+import { ErrorMessage } from "@hookform/error-message"
+import jwt from "jwt-decode"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { useActions } from "../../../hooks/useActions"
+import { useTypedSelector } from "../../../hooks/useTypedSelector"
+import { useLoginUserMutation } from "../../../store/user/userApi"
+import { LoginType } from "../../../store/user/userTypes"
+import { ActionAlert, ActionLoader, InputError } from "../../UI"
+import Popup from "../../UI/Popup/Popup"
+import "../../UI/Popup/Popup.scss"
+import "./SignInPopup.scss"
 
 const SignInPopup = () => {
-    const {onSignInClick} = useActions()
-    const {isSignInModuleOpen} = useTypedSelector(state => state.popup)
-    const [loginUser, {data: token, error, isLoading, isSuccess, isError}] = useLoginUserMutation()
+    const { onSignInClick } = useActions()
+    const { isSignInModuleOpen } = useTypedSelector(state => state.popup)
+    const [loginUser, { data: token, error, isLoading, isSuccess, isError }] = useLoginUserMutation()
 
-    const {login, logout} = useActions()
+    const { login, logout } = useActions()
 
     const {
         register,
-        formState: {errors},
+        formState: { errors },
         handleSubmit,
         reset
-    } = useForm<LoginType>({mode: 'onBlur'})
+    } = useForm<LoginType>({ mode: 'onBlur' })
 
     const onSubmit: SubmitHandler<LoginType> = async (data) => {
         loginUser(data)
         reset()
-        await checkToken()
         onSignInClick()
     }
 
-    const checkToken = () => {
-        const decode: string = jwt(token);
+    if (isSuccess) {
+        const decode: string = jwt(token.access_token)
         login(decode)
     }
-
-    console.log(token)
 
     return (
         <>
@@ -45,43 +42,43 @@ const SignInPopup = () => {
                     <div className="signin-popup__inner">
                         <div className="signin-popup__contacts">
                             <button className="popup__close signin-popup__close"
-                                    onClick={() => onSignInClick()}></button>
+                                onClick={() => onSignInClick()}></button>
                             <div className="signin__body">
                                 <div className="signin__title">Кабинет</div>
                                 <form action="#" className="signin__form">
                                     <div className="signin__login">
                                         <label htmlFor="signin__login" className="input-text">Логин</label>
                                         <input type="text"
-                                               id="signin__login"
-                                               className="signin__login input-style"
-                                               {...register('username', {
-                                                   required: 'Поле обязательно к заполнению',
-                                                   minLength: {value: 5, message: 'Минимум 5 символов'},
-                                                   maxLength: {value: 20, message: 'Максимум 20 символов'}
-                                               })}
+                                            id="signin__login"
+                                            className="signin__login input-style"
+                                            {...register('username', {
+                                                required: 'Поле обязательно к заполнению',
+                                                minLength: { value: 5, message: 'Минимум 5 символов' },
+                                                maxLength: { value: 20, message: 'Максимум 20 символов' }
+                                            })}
                                         />
 
                                         <ErrorMessage name={'username'}
-                                                      errors={errors}
-                                                      render={({message}) => <InputError message={message}/>}
+                                            errors={errors}
+                                            render={({ message }) => <InputError message={message} />}
                                         />
                                     </div>
 
                                     <div className="signin__password">
                                         <label htmlFor="signin__password" className="input-text">Пароль</label>
                                         <input type="password"
-                                               id="signin__password"
-                                               className="signin__login input-style"
-                                               {...register('password', {
-                                                   required: 'Поле обязательно к заполнению',
-                                                   minLength: {value: 5, message: 'Минимум 5 символов'},
-                                                   maxLength: {value: 20, message: 'Максимум 20 символов'}
-                                               })}
+                                            id="signin__password"
+                                            className="signin__login input-style"
+                                            {...register('password', {
+                                                required: 'Поле обязательно к заполнению',
+                                                minLength: { value: 5, message: 'Минимум 5 символов' },
+                                                maxLength: { value: 20, message: 'Максимум 20 символов' }
+                                            })}
                                         />
 
                                         <ErrorMessage name={'password'}
-                                                      errors={errors}
-                                                      render={({message}) => <InputError message={message}/>}
+                                            errors={errors}
+                                            render={({ message }) => <InputError message={message} />}
                                         />
                                     </div>
                                 </form>
@@ -95,10 +92,10 @@ const SignInPopup = () => {
                 </form>
             </Popup>
 
-            {isLoading && <ActionLoader/>}
-            {isSuccess && <ActionAlert message={'Вы успешно вошли в аккаунт'} success={true}/>}
+            {isLoading && <ActionLoader />}
+            {isSuccess && <ActionAlert message={'Вы успешно вошли в аккаунт'} success={true} />}
         </>
-    );
-};
+    )
+}
 
-export default SignInPopup;
+export default SignInPopup
