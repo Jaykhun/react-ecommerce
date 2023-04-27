38e569fad543c449a70d417b90f2063dac4810ec
jaykhun <dosibaevjaykhun@gmail.com>
2023-04-27 18:39:04 +0300

    fix vercel

---
+++ src/pages/Product/ProductDetails/LoaderDetails.tsx
@@ -0,0 +1,11 @@
+import Loader from 'react-loading-skeleton'
+
+const LoaderDetails = () => {
+  return (
+    <div>
+      <Loader width={300} height={350} borderRadius={6} />
+    </div>
+  )
+}
+
+export default LoaderDetails
